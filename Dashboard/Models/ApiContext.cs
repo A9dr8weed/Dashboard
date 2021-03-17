db59dfe1e6b923278fef1de9c3630cd07815da3d
Andrew <andsof123455@gmail.com>
2021-03-17 17:17:10 +0200

    Add model

---
+++ Dashboard/Models/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dashboard.Models
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+    }
+}
---
+++ Dashboard/Models/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace Dashboard.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
