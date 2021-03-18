285eee703c2a12f787c3100ea8a4287bae5c633a
Andrew <andsof123455@gmail.com>
2021-03-18 10:08:48 +0200

    OrderControllers

--- Dashboard/Controllers/OrderController.cs
+++ Dashboard/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Dashboard.Controllers
@@ -36,6 +37,50 @@ namespace Dashboard.Controllers
             };
 
             return Ok(response);
+        }
+
+        [HttpGet("ByState")]
+        public IActionResult ByState()
+        {
+            // Отримуємо всі Orders
+            List<Order> orders = _db.Orders.Include(o => o.Customer).ToList();
+
+            var groupedResult = orders
+                .GroupBy(o => o.Customer.State)
+                .ToList()
+                .Select(grp => new { State = grp.Key, OrderTotal = grp.Sum(x => x.OrderTotal) })
+                .OrderByDescending(res => res.OrderTotal)
+                .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("ByCustomer/{n}")]
+        public IActionResult ByCustomer(int n)
+        {
+            // Отримуємо всі Orders
+            List<Order> orders = _db.Orders.Include(o => o.Customer).ToList();
+
+            var groupedResult = orders.GroupBy(o => o.Customer.Id)
+                .ToList()
+                .Select(grp => new
+                {
+                    Name = _db.Customers.Find(grp.Key).Name,
+                    OrderTotal = grp.Sum(x => x.OrderTotal)
+                })
+                .OrderByDescending(res => res.OrderTotal)
+                .Take(n)
+                .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("GetOrder/{}", Name = "GetOrder")]
+        public IActionResult GetOrder(int id)
+        {
+            Order order = _db.Orders.Include(o => o.Customer).First(o => o.Id == id);
+
+            return Ok(order);
         }
     }
 }
